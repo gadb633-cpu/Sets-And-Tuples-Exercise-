9a82dee9e014ca6ce5def3b0bdc93b6b19feab98
gad-biton <gadb633@gmail.com>
2026-06-23 16:43:09 +0300

    added part_1 4

--- collections_practice.py
+++ collections_practice.py
@@ -11,3 +11,13 @@ tags.discard("bash")
 print(tags)
 tags.discard("banana")
 print(tags)
+# 4
+a = {1,2,3}
+b = {3,4,5}
+print(a | b)
+print(a & b)
+diff_a = a - b
+diff_b = b - a
+print(diff_a)
+print(diff_b)
+
